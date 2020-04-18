c9108f2d372fadcb1411739c6d2134471506ffb3
fbarbera <fbarbera@hexacta.com>
2020-04-18 17:03:11 -0300

    Una forma de usar el store

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Create Events</h1>
+  <div>
+    <h1>Create an Event, {{ userName }}</h1>
+    <p>This event was created by {{ userId }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: mapState({
+    userName: state => state.user.name,
+    userId: state => state.user.id,
+    categories: state => state.categories
+  })
+}
+</script>
+
+<style lang="scss" scoped></style>
